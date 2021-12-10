ad53756c48381f56bf1014af0fce6dc89fbb3e2d
Cole Thiessen <colekthiessen@gmail.com>
2021-12-09 20:58:51 -0700

    Implemented backtester. Plots and calcs mse

--- linear_regression.py
+++ linear_regression.py
@@ -2,6 +2,7 @@ import numpy as np
 import yfinance as yf
 import pandas as pd
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
 import os
 import bs4 as bs
 import requests
@@ -14,13 +15,9 @@ def add_stocks_from_tickers(tickers: list[str], **kwargs: dict[str, str]) -> pd.
     #fetching-data-for-multiple-tickers to see how they work.
     Takes optional period and interval keyword args. Go to https://github.com/ranaroussi/yfinance
     """
-    # close_prices = yf.download(tickers, period=kwargs.get('period', "10y"), interval=kwargs.get(
-    #     'interval', "1d"))[['Close']].dropna()
 
     joint_stock_df = pd.DataFrame()
     date = datetime.datetime(2014,12,3,0,0,0)
-    # close_prices = yf.download(tickers, start="2012-12-03", end="2021-12-01", interval=kwargs.get(
-    #         'interval', "1d"))[['Close']].dropna()
     for ticker in tickers: 
         close_prices = yf.download(ticker, start="2014-12-03", end="2021-12-01", interval=kwargs.get(
             'interval', "1d"))[['Close']].dropna()
@@ -51,72 +48,71 @@ def add_factors_from_tickers(factors: list[str], **kwargs: dict[str, str]):
     return add_stocks_from_tickers(factors, **kwargs)
 
 def split_data(df: pd.DataFrame, ratio = 0.7) -> tuple[pd.DataFrame, pd.DataFrame]:
-  return df.iloc[:int(ratio*len(df))], df.iloc[:int((1-ratio)*len(df))]
+  return df.iloc[:int(ratio*len(df))], df.iloc[int(ratio*len(df)):]
 
-def test_model(model: sm.OLS, factor_test_df: pd.DataFrame, stock_test_df):
-    print(factor_test_df)
-    for index, row in factor_test_df.iterrows():
-        factor_arr = row.values
-        print(factor_arr)
-        prediction = model.predict(factor_arr)
-        print(prediction)
-        exit()
-      
+def test_model(model: sm.OLS, factor_test_df: pd.DataFrame, stock_test_df: pd.Series, debug = False):
+    prediction: pd.Series = model.predict(factor_test_df)
 
-def regress_factors(stocks_df: pd.DataFrame, factors_df: pd.DataFrame):
+    prediction_and_actual = prediction.to_frame('Prediction').join(stock_test_df, on='Date', how='left', lsuffix='_left', rsuffix='_right')
+
+    prediction_and_actual['squared_error'] = (prediction_and_actual[stock_test_df.name] - prediction_and_actual['Prediction']) ** 2
+
+    if debug: print(prediction_and_actual)
+
+    mse = prediction_and_actual['squared_error'].sum() / prediction.size
+    print(f"Prediction Mean Squared Error: {mse}")
+
+    prediction_and_actual.plot()
+    plt.show()
+
+
+def regress_factors(stocks_df: pd.DataFrame, factors_df: pd.DataFrame, signif_level = 0.05, r2_threshold = 0.7):
     """Takes a list of factors that you want to regress on and will print a summary of a multiple regression on those factors with the objects stock
 
     Can pass in self.factor_names to regress on all factors
     """
-    SIGNIF_LEVEL = 0.05
-    R2THRESHOLD = 0.65
 
-    factors_df.drop('MSFT', axis=1, inplace=True)
     factor_train_df, factor_test_df = split_data(factors_df)
-    stock_train_df, stock_test_df = split_data(stocks_df)
-
-    # print(factor_test_df)
-    # print(stocks_df)
 
     portfolios = dict()
     for stock in stocks_df:
-        single_stock_df = stock_train_df[stock]
+        stock_train_df, stock_test_df = split_data(stocks_df[stock])
         pvals_under_sig = False
         temp_factor_df: pd.DataFrame = factor_train_df
     
         while(not(pvals_under_sig) and (len(temp_factor_df.columns)>0)):
 
             ticker = stock[1] if isinstance(stock, tuple) else stock
-            model = sm.OLS(single_stock_df, temp_factor_df)
+            model = sm.OLS(stock_train_df, temp_factor_df)
             results = model.fit()
-            # print(results.summary())
 
             factor_pvals = zip(results.pvalues.index, results.pvalues.values)
             all_pvals_under = True
             for factor_pval in factor_pvals:
                 factor, pvalue = factor_pval
-                if pvalue > SIGNIF_LEVEL:
+                if pvalue > signif_level:
                     all_pvals_under = False
                     temp_factor_df.drop(columns=factor, axis = 1, inplace=True)
 
-            if(all_pvals_under):
+            if results.rsquared_adj >= r2_threshold and all_pvals_under:
+                if (portfolios.get(str(factor))) is None:
+                    portfolios[str(factor)] = set([ticker])
+                else:
+                    portfolios[str(factor)].add(ticker)
+
+                to_remove = [col for col in factor_test_df.columns if col not in temp_factor_df.columns]
+                factor_test_df.drop(to_remove, axis=1, inplace=True)
+                test_model(results, factor_test_df, stock_test_df)
+                print(results.summary())
+
+            if all_pvals_under:
                 pvals_under_sig = True
 
-        if(results.rsquared_adj >= R2THRESHOLD):
-            to_remove = [col for col in factor_test_df.columns if col not in temp_factor_df.columns]
-            factor_test_df.drop(to_remove, axis=1, inplace=True)
-            test_model(model, factor_test_df, stock_test_df)
-            print(results.summary())
-        
     return portfolios
-
-
-
 
 def debug_shape(dfs: list[pd.DataFrame]):
     for df in dfs:
         print(df.shape)
-
 
 def add_factors_from_csv(directory) -> pd.DataFrame:
     """Takes a factor name and a CSV file, calculates the returns and returns them as a dataframe
@@ -139,8 +135,10 @@ def add_factors_from_csv(directory) -> pd.DataFrame:
     return combined_factors.apply(lambda factor: get_returns(factor))
 
 
-def normalizeFactorDates(dates: pd.DataFrame, stock_factors: pd.DataFrame) -> pd.DataFrame:
-    return dates.join(stock_factors, on='Date', how='left', lsuffix='_left', rsuffix='_right')
+def normalize_factor_dates(stock_factors: pd.DataFrame) -> pd.DataFrame:
+    temp_stock = add_stocks_from_tickers(['MSFT'])
+    return temp_stock.join(stock_factors, on='Date', how='left', lsuffix='_left', rsuffix='_right') \
+        .drop('MSFT', axis=1)
 
 
 # Retrieving a list of all of the stocks in the S&P 500 index and putting them in a list
@@ -157,7 +155,6 @@ def save_sp500_tickers():
     return tickers
 
 def get_n_random_stocks(num):
-
     randlist = []
     numStocksToAnalyze =num
     i = 0
@@ -175,26 +172,17 @@ def get_n_random_stocks(num):
     return stocks_to_analyze
     
 
-
-kwargs = {'interval': '1mo'}
-
-#temp stock data frame so we can join our factor data with the stock data efficiently. This data frame is not processed.
-tempStock = add_stocks_from_tickers(['MSFT'])
 stocks_to_analyze = get_n_random_stocks(3)
 
 stocks = add_stocks_from_tickers(stocks_to_analyze)
-# stocks = add_stocks_from_tickers(['MSFT'])
 factors = add_factors_from_csv('factorDirectory/')
 
-normalizedFactors = normalizeFactorDates(tempStock, factors)
+normalizedFactors = normalize_factor_dates(factors)
 
 for column in normalizedFactors.columns: 
     if column == 'Close':
         normalizedFactors = normalizedFactors.drop(columns=column, axis = 1)
 
-# debug_shape([normalizedFactors,tempStock])
-debug_shape([normalizedFactors, stocks])
-
 portfolios = regress_factors(stocks, normalizedFactors)
-# print(portfolios)
+print(portfolios)
 
